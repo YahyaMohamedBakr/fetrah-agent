e4287f6a7f55dcbb05770ea63277d9b36ad03d2a
Yahya Bakr <y.bakr1010@gmail.com>
2026-06-08 19:45:55 +0300

    Fix Settings page: use Schema instead of Form for Filament 5.6

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -5,15 +5,12 @@ namespace App\Filament\Pages;
 use App\Models\Setting;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
+use Filament\Schemas\Schema;
 
 class Settings extends Page
 {
-    use InteractsWithForms;
-
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-cog-6-tooth';
 
     public ?array $data = [];
@@ -29,9 +26,9 @@ class Settings extends Page
         ]);
     }
 
-    public function form(Form $form): Form
+    public function form(Schema $schema): Schema
     {
-        return $form
+        return $schema
             ->schema([
                 TextInput::make('site_name')->label('اسم الموقع')->required(),
                 Textarea::make('site_description')->label('وصف الموقع'),
